d3f28b66319c32f1635c50a679edc2e3c0b272bc
Ryan Hewitt <ryan.hewitt@badunetworks.com>
2016-06-05 16:33:11 -0700

    register models with admin

--- dropzone/logbook/admin.py
+++ dropzone/logbook/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Aircraft, Parachute, Container, Rig
 
 # Register your models here.
+admin.site.register(Aircraft)
+admin.site.register(Parachute)
+admin.site.register(Container)
+admin.site.register(Rig)
